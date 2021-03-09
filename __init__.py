7334139c88cf8ca4716f516b4e807548c905645b
fengfeng <huangfeng191@163.com>
2021-03-09 17:30:26 +0800

    complete yaml

--- __init__.py
+++ __init__.py
@@ -14,19 +14,10 @@ def yaml_deal(plan=None):
 
 
 if __name__ == '__main__':
-    # clear_filename(path="F:\cs",prefix="3")
-    # clear_filename(path="F:/annie/java_basic", prefix=u"教程-java入门必备-适合初学者的全套完整版教程")
-    # clear_filename(path=u"F:\\annie\\尚硅谷\\Spring注解驱动教程(雷丰阳源码级讲解)", prefix=u"尚硅谷_")
-    # get_duration(path=u"E:\\annie\\spring security",rule={"tp":"num","st":20,"end":35})
 
     first_cmd=None
     
-    # print(111000000000000000001)
-    # print(first_cmd)
 
-    # path st end
-
-    # command param1 param2 param3
 
     command=None
     params={}
